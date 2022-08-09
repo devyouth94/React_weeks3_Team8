df82f8ade2f4d14eb7fb937b29080927afe389c3
shkiimaa <shkiimaa@gmail.com>
2022-08-09 16:11:23 +0900

    #6 홈화면 게시글 get

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { _getArticle } from "../redux/postslice"
 
 const Home = () => {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+  useEffect(() => { dispatch(_getArticle()) }, []);
+
+  const state = useSelector(state => state.posts.article);
+
+  return (
+    <div>
+      {state.map((value) => (
+        <div>
+          <h2 key={ "k" + value.id }>{value.title}</h2>
+        </div>
+      ))
+      }
+    </div >
+  )
 };
 
 export default Home;
