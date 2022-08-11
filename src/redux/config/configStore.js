bea666ef0d55db02a7aaedf4cdd7ec3c13c50570
devyouth94 <devyouth94@gmail.com>
2022-08-11 10:34:40 +0900

    배포 환경시 devTools 비활성화

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -9,6 +9,8 @@ const store = configureStore({
     posts,
     comments: commentsSlice,
   },
+  // 배포 환경일때, devTools가 false가 됩니다.
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export default store;
