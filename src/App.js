73a0a7e69d7fa9e864c266582621ebe122222747
devyouth94 <devyouth94@gmail.com>
2022-08-11 10:10:19 +0900

    배포 환경시 콘솔창 깔끔하게

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import Router from "./shared/Router";
 
 const App = () => {
-  return (
-    <>
-      <Router />
-    </>
-  );
+  if (process.env.NODE_ENV === "production") {
+    console.log = function no_console() {};
+    console.warn = function no_console() {};
+  }
+  return <Router />;
 };
 
 export default App;
